d0e4407f43ffe4c778e6252a526b6684de5decc7
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2023-05-08 02:33:54 +0530

    update api url

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build --no-cache -t anilyerramasu/radtel-pacs-ui .
-docker run -it --rm -p 3000:80 anilyerramasu/radtel-pacs-ui /bin/bash
+docker build --no-cache -t anilyerramasu/ris-ui .
+docker run -it --rm -p 3000:80 anilyerramasu/ris-ui /bin/bash
 docker run  --rm -p 3000:80 anilyerramasu/radtel-pacs-ui
-docker push anilyerramasu/pacs-ui
+docker push anilyerramasu/ris-ui
