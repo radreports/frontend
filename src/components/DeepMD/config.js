e1efec2b198626b1326210c01304287eaf587eb0
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-05-04 12:26:41 +0530

    Added totalseg

--- src/components/DeepMD/config.js
+++ src/components/DeepMD/config.js
@@ -7,8 +7,8 @@
 const config = {
     // API_URL: "http://localhost:8888/api/v1",
     API_URL: "https://api.deepmd.io/api/v1",
-    // EHR_URL: "https://ehr.radassist.ai/hapi-fhir-jpaserver/fhir",
-    EHR_URL: "http://34.170.63.108:8080/hapi-fhir-jpaserver/fhir",
+    EHR_URL: "https://ehr.radassist.ai/hapi-fhir-jpaserver/fhir",
+    // EHR_URL: "http://34.170.63.108:8080/hapi-fhir-jpaserver/fhir",
     // API_URL: "http://104.171.202.250:8081",
     // API_URL: "http://localhost:8081",
     // API_URL: "http://localhost:8888/api/v1",
