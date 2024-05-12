7ded63e66c7ad4fcd844d2d51e9b3af9cf3c172a
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-05-12 15:36:56 +0530

    Emhanced observationpage

--- src/components/DeepMD/ObservationPage.js
+++ src/components/DeepMD/ObservationPage.js
@@ -3,52 +3,28 @@ import axios from 'axios';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import config from "./config";
-import FHIR from "fhirclient"
 
 const ObservationPage = ({ diagnosticReportId }) => {
   const [observations, setObservations] = useState([]);
   const EHR_URL = config.EHR_URL;
-  const client = new FHIR.client(EHR_URL);
-  console.log("diagnosticReportId is ::",diagnosticReportId);
-//   useEffect(() => {
-//     const EHR_URL = config.EHR_URL;
-//     // const client = new FHIR.client(" http://hapi.fhir.org/baseR4/");
-//     const client = new FHIR.client(EHR_URL);
-//     const getPath = client.getPath;
-//     var my_resource = [] ;
-//     client.request(`/DiagnosticReport/${diagnosticReportId}`, {
-//         pageLimit: 0 ,
-//         // resolveReferences: [ "subject","result","study"]
-//     })
-//     .then(data => {
-//         console.log("From diagnostic report details ::",data);
-//         try{
-//             // const results = data[0].entry;
-            
-//         }
-//         catch(e){}
-//     })
-//     .catch(err => {
-//         console.log(err);
-//      });
 
-// },[]);
-    
   useEffect(() => {
     const fetchObservations = async () => {
       try {
         const reportResponse = await axios.get(`${EHR_URL}/DiagnosticReport/${diagnosticReportId}`);
-        console.log(reportResponse.data.result);
-        const observationLinks = reportResponse.data.result; // Adjust according to the FHIR resource structure
-        console.log("observationLinks::",observationLinks);
+        const observationLinks = reportResponse.data.result || []; // Ensuring there's a fallback
         const observationPromises = observationLinks.map(link =>
-            
-            axios.get(`${EHR_URL}/${link.reference}`) // Assumes full URL needs to be constructed
+          axios.get(`${EHR_URL}/${link.reference}`)
         );
-
         const observationResults = await Promise.all(observationPromises);
-        console.log("observationResults",observationResults);
-        setObservations(observationResults.map(res => res.data));
+        const formattedData = observationResults.map(res => ({
+          id: res.data.id,
+          typeCode: res.data.code.coding[0].code,  // Accessing the first coding entry
+          typeDisplay: res.data.code.coding[0].display,
+          value: res.data.valueQuantity.value,
+          unit: res.data.valueQuantity.unit
+        }));
+        setObservations(formattedData);
       } catch (error) {
         console.error('Failed to fetch data:', error);
       }
@@ -56,30 +32,28 @@ const ObservationPage = ({ diagnosticReportId }) => {
 
     fetchObservations();
   }, [diagnosticReportId]);
+
   const header = (
     <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-      <h5 className="m-0"> Radiomic Feature</h5>
-      {/* <span className="block mt-2 md:mt-0 p-input-icon-left">
-                <i className="pi pi-search" />
-                <InputText type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Search..." />
-            </span> */}
+      <h5 className="m-0">Radiomic Feature</h5>
     </div>
   );
+
   return (
     <div className="grid crud-demo">
       <div className="col-12">
         <div className="card">
-      <DataTable value={observations}
-      className="datatable-responsive"
-      header={header}
-      responsiveLayout="scroll"
-      >
-        <Column field="id" header="ID"></Column>
-        <Column field="code.text" header="Type"></Column>
-        <Column field="valueQuantity.value" header="Value"></Column>
-        <Column field="valueQuantity.unit" header="Unit"></Column>
-      </DataTable>
-      </div>
+          <DataTable value={observations}
+                     className="datatable-responsive"
+                     header={header}
+                     responsiveLayout="scroll">
+            <Column field="id" header="ID"></Column>
+            <Column field="typeCode" header="Code"></Column>
+            <Column field="typeDisplay" header="Description"></Column>
+            <Column field="value" header="Value"></Column>
+            <Column field="unit" header="Unit"></Column>
+          </DataTable>
+        </div>
       </div>
     </div>
   );
