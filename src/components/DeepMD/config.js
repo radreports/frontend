8e31fe70facdd4a7f9e27f4fde80a3b6e9f5784b
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-08-16 18:58:20 +0530

    Added chat

--- src/components/DeepMD/config.js
+++ src/components/DeepMD/config.js
@@ -5,8 +5,8 @@
 //  }
 
 const config = {
-    API_URL: "http://localhost:8888/api/v1",
-    // API_URL: "https://api.deepmd.io/api/v1",
+    // API_URL: "http://localhost:8888/api/v1",
+    API_URL: "https://api.deepmd.io/api/v1",
     EHR_URL: "https://ehr.radassist.ai/fhir",
     // EHR_URL: "http://34.170.63.108:8080/fhir",
     // API_URL: "http://104.171.202.250:8081",
